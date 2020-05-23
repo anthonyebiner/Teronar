3703812862b8072806610bb42adf736133febead
Anthony Ebiner <anthonyebiner@berkeley.edu>
2020-05-23 09:58:04 -0700

    Initial commit

---
+++ android/src/com/bears/teronar/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bears.teronar;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bears.teronar.Teronar;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Teronar(), config);
+	}
+}
---
+++ desktop/src/com/bears/teronar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bears.teronar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bears.teronar.Teronar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Teronar(), config);
+	}
+}
