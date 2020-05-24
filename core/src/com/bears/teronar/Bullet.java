b2d29fc184b0764b5b2178d70683941cd8b9a24d
matthew29tang <matthew29tang@gmail.com>
2020-05-23 18:04:31 -0700

    Directional shooting

--- core/src/com/bears/teronar/Bullet.java
+++ core/src/com/bears/teronar/Bullet.java
@@ -15,11 +15,12 @@ public class Bullet {
     final Teronar game;
     private int cooldown;
     float x, y;
+    private int orientation;
 
     public static final int SPEED = 500;
     public boolean remove = false;
 
-    public Bullet(Texture texture, final Teronar game) {
+    public Bullet(Texture texture, final Teronar game, int orientation) {
         this.texture = texture;
         this.movementSpeed = Speed.MEDIUM;
         this.solid = true;
@@ -29,16 +30,37 @@ public class Bullet {
         this.cooldown = 0;
         this.x = game.centerX;
         this.y = game.centerY;
+        this.orientation = orientation;
     }
 
     public void update (float deltaTime) {
-        x += SPEED * deltaTime;
-        if (x > game.screenSizeX)
-            remove = true;
+        if (this.orientation == 0) {
+            x += SPEED * deltaTime;
+            if (x > game.screenSizeX)
+                remove = true;
+        } else if (this.orientation == 1) {
+            y += SPEED * deltaTime;
+            if (y < 0)
+                remove = true;
+        } else if (this.orientation == 2) {
+            x -= SPEED * deltaTime;
+            if (x < 0)
+                remove = true;
+        } else if (this.orientation == 3) {
+            y -= SPEED * deltaTime;
+            if (y > game.screenSizeY)
+                remove = true;
+        }
     }
 
     public void render() {
-        game.batch.draw(this.texture, x, y);
+        int rotate = 0;
+        if (this.orientation == 1) {
+            rotate = 90;
+        } else if (this.orientation == 3) {
+            rotate = -90;
+        }
+        game.batch.draw(this.texture, x, y, 32, 32, 64, 64, 1, 1, rotate, 0, 0, 64, 64, false, false);
 
     }
 }
--- core/src/com/bears/teronar/Character.java
+++ core/src/com/bears/teronar/Character.java
@@ -11,7 +11,8 @@ public class Character {
     private Texture bulletTexture;
     private ArrayList<Bullet> movingBullets;
     long cooldown;
-    private final int MIN_COOLDOWN = 250;
+    private final int MIN_COOLDOWN = 350;
+    private int orientation = 0;
 
     public Character(final Teronar game, Texture mainCharacterTexture, Texture projectile) {
         this.character = new Actor(mainCharacterTexture);
@@ -24,10 +25,20 @@ public class Character {
     public void render(float delta) {
         game.batch.draw(character.texture, game.centerX, game.centerY);
         long cooldownDiff =  System.currentTimeMillis() - cooldown;
-        System.out.println(cooldownDiff);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            this.orientation = 0;
+        else if (Gdx.input.isKeyPressed(Input.Keys.UP))
+            this.orientation = 1;
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            this.orientation = 2;
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
+            this.orientation = 3;
+
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && cooldownDiff > MIN_COOLDOWN) {
+            System.out.println(this.orientation);
             this.cooldown = System.currentTimeMillis();
-            movingBullets.add(new Bullet(bulletTexture, game));
+            movingBullets.add(new Bullet(bulletTexture, game, this.orientation));
         }
 
         ArrayList<Bullet> bulletsToRemove = new ArrayList<Bullet>();
