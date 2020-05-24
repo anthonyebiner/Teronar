a2471c37a331024c0d85c171bf7bf4720c65cb0d
Anthony Ebiner <anthonyebiner@berkeley.edu>
2020-05-23 18:59:34 -0700

    fix collision

--- core/src/com/bears/teronar/Level.java
+++ core/src/com/bears/teronar/Level.java
@@ -52,7 +52,8 @@ public class Level {
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
             game.centerY -= 200 * Gdx.graphics.getDeltaTime();
 
-        if (getTile(game.centerX, game.centerY).solid || getTile(game.centerX+64, game.centerY+64).solid) {
+        if (getTile(game.centerX, game.centerY).solid || getTile(game.centerX+64, game.centerY+64).solid
+        || getTile(game.centerX+64, game.centerY).solid || getTile(game.centerX, game.centerY+64).solid) {
             game.centerX = oldx;
             game.centerY = oldy;
         }
@@ -105,13 +106,14 @@ public class Level {
         actors.add(new Enemy(game, game.getTexture("assets/pot_health.png"), new Position(300, 250)));
         actors.add(new Enemy(game, game.getTexture("assets/pot_health.png"), new Position(800, 60)));
         Tile[][] tiles = {
-                {w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w},
-                {f11, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1},
-                {f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3},
-                {f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2},
-                {f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1},
-                {f3, f3, f3, f3, f31, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3},
-                {f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f21}
+                {w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w},
+                {w, f11, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, w},
+                {w, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, w},
+                {w, f2, f2, f2, f2, w, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, w, f2, f2, f2, w},
+                {w, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, f1, w, f1, f1, f1, f1, f1, f1, f1, f1, f1, w},
+                {w, f3, f3, f3, f3, f31, f3, f3, f3, f3, f3, f3, f3, f3, f3, f3, w, f3, f3, f3, f3, f3, f3, f3, f3, f3, w},
+                {w, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f2, f21, w},
+                {w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w},
         };
         return new Level(game, tiles, actors);
     }
