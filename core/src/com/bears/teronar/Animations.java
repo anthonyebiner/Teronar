198f9a31280bff43652b00403485618641719a70
Shru <shruteek@gmail.com>
2020-05-24 03:40:42 -0700

    Made several edits to Animations to allow for nonrepeating animations.

--- core/src/com/bears/teronar/Animations.java
+++ core/src/com/bears/teronar/Animations.java
@@ -1,17 +1,21 @@
 package com.bears.teronar;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import java.util.ArrayList;
 
 public class Animations {
     private ArrayList<TextureRegion> frames;
+    private static TextureRegion empty = new TextureRegion(new Texture("./assets/empty.png"));
     private float maxFrameTime;
     private float currentFrameTime;
     private int frameCount;
     private int frame;
+    private Boolean playOnce; // This is a boolean that defines if the animation should not repeat.
 
     public Animations(TextureRegion region, int frameCount, float cycleTime) {
+        playOnce = true;
         frames = new ArrayList<TextureRegion>();
         int frameWidth = region.getRegionWidth() / frameCount;
         for(int i = 0; i < frameCount; i++) {
@@ -22,19 +26,29 @@ public class Animations {
         frame = 0;
     }
 
-    public void update(float delta) {
+    public void update(float delta) { // Update the animation based on how much time has passed.
         currentFrameTime += delta;
-        if(currentFrameTime > maxFrameTime){
+        if (currentFrameTime > maxFrameTime){
             frame++;
             currentFrameTime = 0;
         }
-        if(frame >= frameCount) {
-            frame = 0;
+
+        if(frame >= frameCount) { // If the animation is at its end...
+            if (playOnce) {
+                frame = frameCount + 1; // If the animation should only be played once, keep it at its end.
+            } else {
+                frame = 0; // Otherwise, restart the animation.
+            }
         }
     }
 
     public TextureRegion getFrame() {
-        return frames.get(frame);
+        if (frame == (frameCount + 1) && playOnce){
+            return empty; // If the animation is to be played once and has played, return an empty frame.
+        } else {
+            return frames.get(frame); // Otherwise, return the current frame.
+        }
+
     }
 
 }
