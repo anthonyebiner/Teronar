64e026b24dab522049a695063e95d2372234a7ce
Anthony Ebiner <anthonyebiner@berkeley.edu>
2020-05-24 10:21:04 -0700

    add intro text

--- core/src/com/bears/teronar/MainMenuScreen.java
+++ core/src/com/bears/teronar/MainMenuScreen.java
@@ -27,8 +27,36 @@ public class MainMenuScreen implements Screen {
 
         game.batch.begin();
 
-        game.font.draw(game.batch, "Welcome to Teronar", game.screenSizeX/2-100, game.screenSizeY/2);
-        game.font.draw(game.batch, "Press any key to begin", game.screenSizeX/2-110, game.screenSizeY/2-20);
+        game.font.draw(game.batch, "Welcome to Teronar", game.screenSizeX/2-100, game.screenSizeY/2+200);
+        game.font.draw(game.batch, "The Medieval Age is at its peak in the " +
+                "magic-filled world of Elysia, where legions of steel knights" +
+                " and arcane sorcerers \nare fighting side-by-side on the " +
+                "battlefield as the rulers of the land wage wars of conquest " +
+                "against each other to establish \ntheir dominance.\n" +
+                "\n" +
+                "Yet one nation has distanced itself from such volatile and " +
+                "deadly conflicts, its monarchy believing these struggles to \n" +
+                "be futile: the Kingdom of Teronar. As nationalism spreads " +
+                "throughout the continent, tensions between the kingdom’s \n" +
+                "people and those of neighboring lands continue to mount. " +
+                "Finally, angered by the unwavering neutrality of the \n" +
+                "government, a group of treacherous citizens revolt against " +
+                "the monarchy.\n" +
+                "\n" +
+                "Armed with black market weapons and aided by sympathizers " +
+                "from the kingdom’s army, the rebels storm the royal palace. \n" +
+                "While the loyal members of the guard bravely rush to the " +
+                "gates to fend off the attackers, defectors overpower the \n" +
+                "royal protective escorts and take the monarch and their " +
+                "family to be executed in the dungeons deep below the palace." +
+                " \n" +
+                "\n" +
+                "Just when all hope seems lost, miraculously, the Grand " +
+                "Sorcerer of Teronar teleports into the dungeons to rescue \n" +
+                "the monarch, unshackling them and desperately casting a " +
+                "powerful reality-bending spell just as the defectors rush to \n" +
+                " eliminate the threat… ", game.screenSizeX/2-400, game.screenSizeY/2+130);
+        game.font.draw(game.batch, "Press any key to begin", game.screenSizeX/2-110, game.screenSizeY/2-200);
         game.batch.end();
 
         if (Gdx.input.isTouched() || Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY)) {
--- html/src/com/bears/teronar/client/HtmlLauncher.java
+++ html/src/com/bears/teronar/client/HtmlLauncher.java
@@ -10,7 +10,7 @@ public class HtmlLauncher extends GwtApplication {
         // USE THIS CODE FOR A FIXED SIZE APPLICATION
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(1024, 576);
         }
         // END CODE FOR FIXED SIZE APPLICATION
 
